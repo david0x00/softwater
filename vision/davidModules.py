50c1bfd74d894a4f6f071c5986c286e2673a8a22
David Null <null2@illinois.edu>
2021-04-20 15:07:31 -0500

    changes so that it spits out CSV

--- vision/davidModules.py
+++ vision/davidModules.py
@@ -2,8 +2,10 @@ import csv
 import os
 import cv2
 import mainvision
+import numpy as np
 
-csv_headers = ["M1X", "M1Y", "M1T", 
+csv_headers = ["M0X", "M0Y", "M0T", 
+               "M1X", "M1Y", "M1T", 
                "M2X", "M2Y", "M2T",
                "M3X", "M3Y", "M3T",
                "M4X", "M4Y", "M4T",
@@ -12,8 +14,7 @@ csv_headers = ["M1X", "M1Y", "M1T",
                "M7X", "M7Y", "M7T",
                "M8X", "M8Y", "M8T",
                "M9X", "M9Y", "M9T",
-               "M10X", "M10Y", "M10T",
-               "M11X", "M11Y", "M11T"]
+               "M10X", "M10Y", "M10T"]
 
 def writeHeaders(file_name):
     with open(file_name, 'a', newline='') as file:
@@ -31,12 +32,94 @@ def imageCount(directory):
         img_list[idx] = int(img_name.split(".")[0])
     return max(img_list)
 
-# The Key function: analyzes images and gets positions and angles
-def analyzeImage(img_name):
-    img = cv2.imread(img_name)
-    cv2.imshow("raw", img)
+def showImage(title, img):
+    cv2.imshow(title, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def getRedMask(image):
+    lower = np.array([120,97,148])
+    upper = np.array([255,255,255])
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    blur_img = cv2.GaussianBlur(hsv_img, (25,25), 0)
+    red_mask = cv2.inRange(blur_img, lower, upper)
+    red_mask = cv2.erode(red_mask, None, iterations=3)
+    red_mask = cv2.dilate(red_mask, None, iterations=3)
+    return red_mask
+
+# The Key function: analyzes images and gets positions and angles
+def analyzeImage(img_name):
+    error_detected = False
+    img = cv2.imread(img_name)
+    red_mask = getRedMask(img)
+
+    edged = red_mask.copy()
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    num_of_points = len(contours)
+
+    if num_of_points != 11:
+        print("Error detected initially.")
+        print(img_name)
+        print(num_of_points)
+        error_detected = True
+
+    center_list = []
+    for c in contours:
+        ((x,y), radius) = cv2.minEnclosingCircle(c)
+        if x > 250:
+            center_list.append((x,y))
+        
+    center_list.sort(key = lambda x: x[1])
+    center_list.reverse()
+
+    if len(center_list) != 11:
+        print("Error still detected.")
+        print(img_name)
+        print(num_of_points)
+        error_detected = True
+    elif len(center_list) == 11 and error_detected == True:
+        print("error corrected")
+        error_detected = False
+
+    base_point = center_list[0]
+    x_base = base_point[0]
+    y_base = base_point[1]
+
+    row_dict = {}
+    for idx, center_point in enumerate(center_list):
+        x_key = "M" + str(idx) + "X"
+        y_key = "M" + str(idx) + "Y"
+        t_key = "M" + str(idx) + "T"
+
+        x_val = center_point[0] - x_base
+        y_val = y_base - center_point[1]
+        t_val = 0
+
+        row_dict[x_key] = x_val
+        row_dict[y_key] = y_val
+        row_dict[t_key] = t_val
+
+    if error_detected:
+        return {}
+    
+    return row_dict
+
+
+
+    #print("Contours Detected: " + str(len(contours)))
+
+    #max_contour = max(contours, key=cv2.contourArea)
+    #max_moments = cv2.moments(max_contour)
+
+    # Here is the part where we show the circles on the centers.
+    # for c in contours:
+    #     m = cv2.moments(c)
+    #     if m['m00'] > (max_moments['m00']/8):
+    #         ((x,y), radius) = cv2.minEnclosingCircle(c)
+    #         center = (int(m['m10'] / m['m00']), int(m['m01'] / m['m00']))
+    #         #cv2.circle(img, (int(x), int(y)), int(radius), (0,255,68), 10)
+    #         cv2.circle(img, center, 2, (255,0,255), 10)
 
 
 # Computer vision marker analyzer
@@ -57,8 +140,8 @@ def analyzeImages(directory):
         writeRow(file_name, marker_dict)
 
 if __name__ == "__main__":
-    directory = "/media/user1/Data 2000/soft_robotics_experiments/module_2_single_actuator_right/m2_right_actuator_simple"
-    #analyzeImages(directory)
-    img_name = directory + "/1.jpg"
-    analyzeImage(img_name)
+    directory = "/media/user1/Data 2000/soft_robotics_experiments/module_2_single_actuator_right/m2_right_actuator_simple3"
+    analyzeImages(directory)
+    #img_name = directory + "/1.jpg"
+    #analyzeImage(img_name)
 
