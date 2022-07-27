b89f3c4a5e15cce59a59696a05ea9418098cd5d6
peempeem <31141746+peempeem@users.noreply.github.com>
2022-07-27 14:19:42 -0500

    removed imports for hover

--- softwater_app/util/kivyclasses.py
+++ softwater_app/util/kivyclasses.py
@@ -1,4 +1,3 @@
-from tkinter.messagebox import RETRY
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -6,7 +5,6 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.behaviors import ButtonBehavior
-from kivymd.uix.behaviors import HoverBehavior
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
