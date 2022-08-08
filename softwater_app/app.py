7ad31e1e2a9ffe263c6ce4419d8a3cd6fff06086
peempeem <31141746+peempeem@users.noreply.github.com>
2022-08-08 15:51:28 -0500

    finished ui, working on comms

--- softwater_app/app.py
+++ softwater_app/app.py
@@ -4,13 +4,12 @@ Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.core.window import Window
-from kivy.clock import Clock
+from kivy.uix.popup import Popup
 
+import os
 import cv2
 import numpy as np
+import json
 
 from util.content_classes import *
 from util.colortools import *
@@ -36,25 +35,33 @@ class MainWindow(App):
         Window.size = min_window
         Window.minimum_width, Window.minimum_height = min_window
 
-        window = BoxLayout(orientation="vertical")
-
         content_div_cc = ColorComponent(sunset_orange, sunset_purple, POS_LEFT, POS_RIGHT)
         content_background_cc = ColorComponent(dim_gray, dark_gray, POS_TOP_CENTER, POS_BOT_CENTER)
         select_cc = ColorComponent(sunset_orange, sunset_purple, POS_TOP_LEFT, POS_BOT_RIGHT)
         button_up_cc = ColorComponent(sunset_purple, gunmetal, POS_BOT_LEFT, POS_RIGHT)
         button_down_cc = ColorComponent(sunset_orange, gunmetal, POS_BOT_LEFT, POS_RIGHT)
 
-        main_layout = MainContentPane("./assets/title.png", "./assets/background.jpg")
+        window = BoxLayout(orientation="vertical")
 
-        title = ImagePane("./assets/title.png", size_hint=(0.3, 0.1), pos_hint={"x": 0.05, "top": 0.95})
+        main_layout = MainContentPane("./assets/title.png", "./assets/background.jpg")
+        title_layout = FloatLayout(size_hint=(1, 0.15), pos_hint={"x": 0, "top": 1})
+
+        title = ImagePane("./assets/title.png", size_hint=(0.3, 0.9), pos_hint={"x": 0.05, "center_y": 0.5})
+        settings_button = IconButton("./assets/settings.png", "./assets/settings_pressed.png", size_hint=(0.1, 0.9), pos_hint={"right": 1, "center_y": 0.5})
 
         content_top = BoxLayout(orientation="horizontal", padding=20, spacing=40, size_hint=(1, 0.35), pos_hint={"center_x": 0.5, "y": 0.5})
         content_bottom = BoxLayout(orientation="horizontal", padding=20, spacing=40, size_hint=(1, 0.5), pos_hint={"center_x": 0.5, "y": 0})
+        self.settings_content_pane = HiddenContentPane(size_hint=(0.4, 0.75), pos_hint={"right": 1}, bar_color_component=content_div_cc, background_color_component=content_background_cc)
+        settings_pane = SettingsPane()
 
         controller_select_pane = ContentPane(bar_color_component=content_div_cc, background_color_component=content_background_cc, size_hint=(2, 1))
-        command_center_pane = ContentPane(bar_color_component=content_div_cc, background_color_component=content_background_cc)
+        self.command_center_pane = ContentPane(bar_color_component=content_div_cc, background_color_component=content_background_cc)
         tracker_pane = ContentPane(bar_color_component=content_div_cc, background_color_component=content_background_cc)
-        robot_state_pane = ContentPane(bar_color_component=content_div_cc, background_color_component=content_background_cc, size_hint=(1.5, 1))
+        self.robot_state_pane = ContentPane(bar_color_component=content_div_cc, background_color_component=content_background_cc, size_hint=(1.5, 1))
+
+        controller_select = ControlSelector(button_up_cc, button_down_cc)
+
+        self.command_center = CommandCenter(button_up_cc, button_down_cc)
 
         self.robot_state_image_pane = RobotImagePane(
             "./assets/robot.png",
@@ -70,19 +77,35 @@ class MainWindow(App):
         # asm
         window.add_widget(main_layout)
 
-        main_layout.add_widget(title)
-        
+        main_layout.add_widget(title_layout)
         main_layout.add_widget(content_top)
         main_layout.add_widget(content_bottom)
+        main_layout.add_widget(self.settings_content_pane)
+
+        title_layout.add_widget(title)
+        title_layout.add_widget(settings_button)
 
         content_top.add_widget(controller_select_pane)
-        content_top.add_widget(command_center_pane)
+        content_top.add_widget(self.command_center_pane)
 
         content_bottom.add_widget(tracker_pane)
-        content_bottom.add_widget(robot_state_pane)
+        content_bottom.add_widget(self.robot_state_pane)
 
+        controller_select_pane.add_widget(controller_select)
+        self.command_center_pane.add_widget(self.command_center)
         tracker_pane.add_widget(self.camera_pane)
-        robot_state_pane.add_widget(self.robot_state_image_pane)
+        self.robot_state_pane.add_widget(self.robot_state_image_pane)
+
+        settings_button.add_callback(self._open_settings)
+        self.settings_content_pane.add_widget(settings_pane)
+
+        controller_select.auto_mpc_button.add_callback(controls.auto_mpc)
+        controller_select.pid_button.add_callback(controls.pid)
+        controller_select.open_loop_button.add_callback(controls.open_loop)
+        controller_select.manual_button.add_callback(controls.manual)
+
+        self.command_center.start_button.add_callback(controls.start_experiment)
+        self.command_center.stop_button.add_callback(controls.stop_experiment)
 
         self.camera_pane.camera_view.add_callback(controls.camera_view)
         self.camera_pane.tracker_view.add_callback(controls.tracker_view)
@@ -109,20 +132,125 @@ class MainWindow(App):
         self.robot_state_image_pane.gate.add_callback(controls.gate)
         self.robot_state_image_pane.sensors.add_callback(controls.sensors)
 
+        settings_pane.brightness.add_callback(self._adjust_brightness)
+        settings_pane.contrast.add_callback(self._adjust_contrast)
+        settings_pane.saturation.add_callback(self._adjust_saturation)
+        settings_pane.exposure.add_callback(self._adjust_exposure)
+        settings_pane.r_min.add_callback(self._adjust_r_min)
+        settings_pane.r_max.add_callback(self._adjust_r_max)
+        settings_pane.g_min.add_callback(self._adjust_g_min)
+        settings_pane.g_max.add_callback(self._adjust_g_max)
+        settings_pane.b_min.add_callback(self._adjust_b_min)
+        settings_pane.b_max.add_callback(self._adjust_b_max)
+
         for i in range(6):
             self.robot_state_image_pane.show_pressure(i, 100.4)
 
+        Clock.schedule_interval(self._refresh_image, 1/30)
         Clock.schedule_interval(self._background_tasks, 1/100)
         controls.app = self
+
+        if not os.path.exists("./appdata"):
+            os.mkdir("./appdata")
+
+        if not os.path.exists("./appdata/settings.json"):
+            self.command_center.frequency_text.set  ("1" )
+            self.command_center.duration_text.set   ("10")
+            settings_pane.brightness.set            (50  )
+            settings_pane.contrast.set              (50  )
+            settings_pane.saturation.set            (50  )
+            settings_pane.exposure.set              (-4  )
+            settings_pane.r_min.set                 (0   )
+            settings_pane.r_max.set                 (255 )
+            settings_pane.g_min.set                 (0   )
+            settings_pane.g_max.set                 (255 )
+            settings_pane.b_min.set                 (0   )
+            settings_pane.b_max.set                 (255 )
+            
+            with open("./appdata/settings.json", "w") as f:
+                json.dump(self.settings, f, indent=4)
+                self.settings = json.load(f)
+        else:
+            with open("./appdata/settings.json", "r") as f:
+                self.settings = json.load(f)
+                settings_pane.brightness.set(self.settings["BRIGHTNESS"])
+                settings_pane.contrast.set(self.settings["CONTRAST"])
+                settings_pane.saturation.set(self.settings["SATURATION"])
+                settings_pane.exposure.set(self.settings["EXPOSURE"])
+                settings_pane.r_min.set(self.settings["R MIN"])
+                settings_pane.r_max.set(self.settings["R MAX"])
+                settings_pane.g_min.set(self.settings["G MIN"])
+                settings_pane.g_max.set(self.settings["G MAX"])
+                settings_pane.b_min.set(self.settings["B MIN"])
+                settings_pane.b_max.set(self.settings["B MAX"])
+        
         return window
+    
+    def on_stop(self, *args):
+        with open("./appdata/settings.json", "w") as f:
+            json.dump(self.settings, f, indent=4)
+    
+    def _open_settings(self, pressed):
+        if (pressed):
+            self.settings_content_pane.toggle()
+            if (self.settings_content_pane.is_open()):
+                self.command_center_pane.disabled = True
+                self.robot_state_pane.disabled = True
+            else:
+                self.command_center_pane.disabled = False
+                self.robot_state_pane.disabled = False
+    
+    def _update_log_frequency(self, text):
+        self.settings["LOG FREQUENCY"] = text
+        print("lel1")
+
+    def _update_log_duration(self, text):
+        self.settings["LOG DURATION"] = text
+        print("yeye02")
+    
+    def _adjust_brightness(self, min, value, max):
+        controls.change_cam_settings(cv2.CAP_PROP_BRIGHTNESS, value)
+        self.settings["BRIGHTNESS"] = value
+    
+    def _adjust_contrast(self, min, value, max):
+        controls.change_cam_settings(cv2.CAP_PROP_CONTRAST, value)
+        self.settings["CONTRAST"] = value
+    
+    def _adjust_saturation(self, min, value, max):
+        controls.change_cam_settings(cv2.CAP_PROP_SATURATION, value)
+        self.settings["SATURATION"] = value
+    
+    def _adjust_exposure(self, min, value, max):
+        controls.change_cam_settings(cv2.CAP_PROP_EXPOSURE, value)
+        self.settings["EXPOSURE"] = value
+    
+    def _adjust_r_min(self, min, value, max):
+        self.settings["R MIN"] = value
+    
+    def _adjust_r_max(self, min, value, max):
+        self.settings["R MAX"] = value
+
+    def _adjust_g_min(self, min, value, max):
+        self.settings["G MIN"] = value
+    
+    def _adjust_g_max(self, min, value, max):
+        self.settings["G MAX"] = value
+    
+    def _adjust_b_min(self, min, value, max):
+        self.settings["B MIN"] = value
+    
+    def _adjust_b_max(self, min, value, max):
+        self.settings["B MAX"] = value
 
     def _background_tasks(self, dt):
-        result, image = cam.read()
+        self.camera_pane.image.get_rgb()
+
+    def _refresh_image(self, dt):
+        result, image = controls.display_image()
         if (result):
             self.camera_pane.image.set_image(image)
 
 app = MainWindow()
-cam = cv2.VideoCapture(0)
 
 if __name__ == '__main__':
     app.run()
--- softwater_app/util/content_classes.py
+++ softwater_app/util/content_classes.py
@@ -1,5 +1,159 @@
 from turtle import pos
+from kivy.clock import Clock
 from util.helpful_kivy_classes import *
+
+
+class CommandCenter(BoxLayout):
+    def __init__(self, button_up_cc, button_down_cc, size_hint=(1, 1), pos_hint={"center_x": 0.5, "center_y": 0.5}):
+        super(CommandCenter, self).__init__()
+        self.size_hint = size_hint
+        self.pos_hint = pos_hint
+        self.padding = 20
+        self.spacing = 10
+        self.orientation = "vertical"
+
+        layout1 = BoxLayout(orientation="horizontal", spacing=40)
+        layout3 = BoxLayout(orientation="horizontal", spacing=40)
+        layout4 = BoxLayout(orientation="horizontal", spacing=40)
+
+        robot_status_title_label = ResizableLabel("Robot Status:", 0.4, halign="left")
+        robot_status_label = ResizableLabel("Not Connected", 0.4, halign="right")
+        self.log_button = RoundToggleButton("Log", "Log", button_down_cc, button_up_cc)
+
+        self.start_button = RoundToggleButton("Start", "Start", button_down_cc, button_up_cc)
+        self.stop_button = RoundToggleButton("Stop", "Stop", button_down_cc, button_up_cc)
+
+        self.add_widget(layout1)
+        self.add_widget(self.log_button)
+        self.add_widget(layout3)
+        self.add_widget(layout4)
+
+        layout3_1 = BoxLayout(orientation="horizontal")
+        layout3_2 = BoxLayout(orientation="horizontal")
+
+        self.frequency_text = ResizableTextInput("", 0.4, size_hint=(1, 0.8), input_filter="float")
+        self.duration_text = ResizableTextInput("", 0.4, size_hint=(1, 0.8), input_filter="float")
+
+        layout3_1.add_widget(ResizableLabel("Frequency:", 0.3, size_hint=(0.9, 1)))
+        layout3_1.add_widget(self.frequency_text)
+        layout3_1.add_widget(ResizableLabel("Hz", 0.3, size_hint=(0.5, 1)))
+        
+        layout3_2.add_widget(ResizableLabel("Duration:", 0.3, size_hint=(0.7, 1)))
+        layout3_2.add_widget(self.duration_text)
+        layout3_2.add_widget(ResizableLabel("sec", 0.3, size_hint=(0.5, 1)))
+
+        layout1.add_widget(robot_status_title_label)
+        layout1.add_widget(robot_status_label)
+        layout3.add_widget(layout3_1)
+        layout3.add_widget(layout3_2)
+        
+        layout4.add_widget(self.start_button)
+        layout4.add_widget(self.stop_button)
+
+        self.start_button.add_callback(self._start_pressed)
+        self.stop_button.add_callback(self._stop_presssed)
+    
+    def _start_pressed(self, pressed):
+        if (pressed):
+            self.stop_button.state = "normal"
+
+    def _stop_presssed(self, pressed):
+        if (pressed):
+            self.start_button.state = "normal"
+
+
+class ControlSelector(GridLayout):
+    def __init__(self, button_up_cc, button_down_cc, size_hint=(1, 1), pos_hint={"center_x": 0.5, "center_y": 0.5}):
+        super(ControlSelector, self).__init__()
+        self.size_hint = size_hint
+        self.pos_hint = pos_hint
+        self.padding = 20
+        self.spacing = 10
+        self.cols = 2
+        self.rows = 4
+
+        self.auto_mpc_button = RoundToggleButton("Auto MPC", "Auto MPC", button_down_cc, button_up_cc, size_hint=(0.2, 1))
+        self.pid_button = RoundToggleButton("PID", "PID", button_down_cc, button_up_cc, size_hint=(0.2, 1))
+        self.open_loop_button = RoundToggleButton("Open Loop", "Open Loop", button_down_cc, button_up_cc, size_hint=(0.2, 1))
+        self.manual_button = RoundToggleButton("Manual", "Manual", button_down_cc, button_up_cc, size_hint=(0.2, 1))
+    
+        auto_mpc_selector = FileSelector(button_up_cc, button_down_cc)
+        pid_selector = FileSelector(button_up_cc, button_down_cc)
+        open_loop_selector = FileSelector(button_up_cc, button_down_cc)
+
+        self.add_widget(self.auto_mpc_button)
+        self.add_widget(auto_mpc_selector)
+        self.add_widget(self.pid_button)
+        self.add_widget(pid_selector)
+        self.add_widget(self.open_loop_button)
+        self.add_widget(open_loop_selector)
+        self.add_widget(self.manual_button)
+
+        self.auto_mpc_button.add_callback(self._auto_mpc_pressed)
+        self.pid_button.add_callback(self._pid_pressed)
+        self.open_loop_button.add_callback(self._open_loop_pressed)
+        self.manual_button.add_callback(self._manual_pressed)
+    
+    def _auto_mpc_pressed(self, pressed):
+        if (pressed):
+            self.pid_button.state = "normal"
+            self.open_loop_button.state = "normal"
+            self.manual_button.state = "normal"
+    
+    def _pid_pressed(self, pressed):
+        if (pressed):
+            self.auto_mpc_button.state = "normal"
+            self.open_loop_button.state = "normal"
+            self.manual_button.state = "normal"
+
+    def _open_loop_pressed(self, pressed):
+        if (pressed):
+            self.auto_mpc_button.state = "normal"
+            self.pid_button.state = "normal"
+            self.manual_button.state = "normal"
+
+    def _manual_pressed(self, pressed):
+        if (pressed):
+            self.auto_mpc_button.state = "normal"
+            self.pid_button.state = "normal"
+            self.open_loop_button.state = "normal"
+
+class SettingsPane(BoxLayout):
+    def __init__(self, size_hint=(1, 1), pos_hint={"center_x": 0.5, "center_y": 0.5}):
+        super(SettingsPane, self).__init__()
+        self.size_hint = size_hint
+        self.pos_hint = pos_hint
+        self.orientation = "vertical"
+        self.padding = 10
+        self.spacing = 10
+
+        camera_settings_header = ResizableLabel("Camera Settings", 0.75, size_hint=(1, 2))
+        self.brightness = TextNumberSlider("Brightness", 1, 0, 100, 0, pos_hint={"x": 0})
+        self.contrast = TextNumberSlider("Contrast", 1, 0, 100, 0, pos_hint={"x": 0})
+        self.saturation = TextNumberSlider("Saturation", 1, 0, 100, 0, pos_hint={"x": 0})
+        self.exposure = TextNumberSlider("Exposure", 1, -7, -1, -7, pos_hint={"x": 0})
+
+        tracker_settings_header = ResizableLabel("Tracker Settings", 0.75, size_hint=(1, 2))
+        self.r_min = TextNumberSlider("R Min", 1, 0, 255, 0, pos_hint={"x": 0})
+        self.r_max = TextNumberSlider("R Max", 1, 0, 255, 0, pos_hint={"x": 0})
+        self.g_min = TextNumberSlider("G Min", 1, 0, 255, 0, pos_hint={"x": 0})
+        self.g_max = TextNumberSlider("G Max", 1, 0, 255, 0, pos_hint={"x": 0})
+        self.b_min = TextNumberSlider("B Min", 1, 0, 255, 0, pos_hint={"x": 0})
+        self.b_max = TextNumberSlider("B Max", 1, 0, 255, 0, pos_hint={"x": 0})
+
+        self.add_widget(camera_settings_header)
+        self.add_widget(self.brightness)
+        self.add_widget(self.contrast)
+        self.add_widget(self.saturation)
+        self.add_widget(self.exposure)
+
+        self.add_widget(tracker_settings_header)
+        self.add_widget(self.r_min)
+        self.add_widget(self.r_max)
+        self.add_widget(self.g_min)
+        self.add_widget(self.g_max)
+        self.add_widget(self.b_min)
+        self.add_widget(self.b_max)
 
 
 class CameraImageSelector(CV2Image, FloatLayout):
@@ -12,8 +166,41 @@ class CameraImageSelector(CV2Image, FloatLayout):
         self.rect.opacity = 0
 
         self.selecting = False
+        self.mouse_pos = None
 
         self.add_widget(self.rect)
+        Window.bind(mouse_pos=self.on_mouse_pos)
+
+    def on_mouse_pos(self, window, pos):
+        if self.collide_point(*pos) and self._inbounds(pos):
+            self.mouse_pos = pos
+        else:
+            self.mouse_pos = None
+    
+    def get_rgb(self):
+        w = float(self.img_size[0])
+        h = float(self.img_size[1])
+
+        if w <= 0 or h <= 0 or self.mouse_pos is None:
+            return None
+    
+        pos = (self.pos[0] + (self.size[0] - w) / 2, h + self.pos[1] + (self.size[1] - h) / 2)
+        x = abs((self.mouse_pos[0] - pos[0]) / w)
+        y = abs((self.mouse_pos[1] - pos[1]) / h)
+
+        zx1 = self.zoom[0][0]
+        zy1 = self.zoom[0][1]
+        zx2 = self.zoom[1][0]
+        zy2 = self.zoom[1][1]
+
+        x = int((zx1 + x * (zx2 - zx1)) * self.cv2_img.shape[1])
+        y = int((zy1 + y * (zy2 - zy1)) * self.cv2_img.shape[0])
+
+        if x >= self.cv2_img.shape[1] or y >= self.cv2_img.shape[0]:
+            return None
+
+        bgr = self.cv2_img[y, x]
+        return (bgr[2], bgr[1], bgr[0])
 
     def on_touch_up(self, touch):
         if (self.selecting):
@@ -27,7 +214,7 @@ class CameraImageSelector(CV2Image, FloatLayout):
             if (bw == 0 or bh == 0):
                 return
 
-            pos = (self.pos[0] + (self.size[0] - self.img_size[0]) / 2, h + self.pos[1] + (self.size[1] - self.img_size[1]) / 2)
+            pos = (self.pos[0] + (self.size[0] - w) / 2, h + self.pos[1] + (self.size[1] - h) / 2)
 
             if (bw < 0):
                 x1 = self.rect.pos[0] + bw
@@ -55,15 +242,12 @@ class CameraImageSelector(CV2Image, FloatLayout):
             zx2 = self.zoom[1][0]
             zy2 = self.zoom[1][1]
 
-            zoom[0][0] = zx1 + zoom[0][0] * (zx2 - zx1)
-            zoom[0][1] = zy1 + zoom[0][1] * (zy2 - zy1)
-            zoom[1][0] = zx1 + zoom[1][0] * (zx2 - zx1)
-            zoom[1][1] = zy1 + zoom[1][1] * (zy2 - zy1)
+            zoom[0][0] = int((zx1 + zoom[0][0] * (zx2 - zx1)) * self.cv2_img.shape[1]) / float(self.cv2_img.shape[1])
+            zoom[0][1] = int((zy1 + zoom[0][1] * (zy2 - zy1)) * self.cv2_img.shape[0]) / float(self.cv2_img.shape[0])
+            zoom[1][0] = int((zx1 + zoom[1][0] * (zx2 - zx1)) * self.cv2_img.shape[1]) / float(self.cv2_img.shape[1])
+            zoom[1][1] = int((zy1 + zoom[1][1] * (zy2 - zy1)) * self.cv2_img.shape[0]) / float(self.cv2_img.shape[0])
 
-            self.set_zoom(zoom)    
-
-            
-
+            self.set_zoom(zoom)
 
     def on_touch_down(self, touch):
         if self._inbounds(touch.pos):
@@ -77,7 +261,7 @@ class CameraImageSelector(CV2Image, FloatLayout):
             w, h = self._forcebounds(touch.pos)
             self.rect.width = w
             self.rect.height = h
-    
+        
     def _inbounds(self, pos):
         _pos = (self.pos[0] + (self.size[0] - self.img_size[0]) / 2, self.pos[1] + (self.size[1] - self.img_size[1]) / 2)
         return pos[0] >= _pos[0] and pos[0] <= _pos[0] + self.img_size[0] and pos[1] >= _pos[1] and pos[1] <= _pos[1] + self.img_size[1]
@@ -106,12 +290,16 @@ class CameraPane(BoxLayout):
         self.padding = 10
         self.spacing = 10
 
-        self.button_layout = BoxLayout(orientation="vertical", size_hint=(0.35, 0.7), pos_hint={"x": 0, "center_y": 0.5}, spacing=10)
+        self.button_layout = BoxLayout(orientation="vertical", size_hint=(0.35, 0.9), pos_hint={"x": 0, "center_y": 0.5}, spacing=10)
         self.image = CameraImageSelector(select_cc, size_hint=(1, 0.9), pos_hint={"center_x": 0.5, "center_y": 0.5})
 
         self.camera_view = RoundToggleButton("Camera", "Camera", button_down_cc, button_up_cc)
         self.tracker_view = RoundToggleButton("Tracker", "Tracker", button_down_cc, button_up_cc)
         self.reset = RoundToggleButton("Reset", "Reset", button_down_cc, button_up_cc)
+        self.rgb_layout = BoxLayout(orientation="horizontal", size_hint=(1, 1))
+        self.r = ResizableLabel("R", 0.3)
+        self.g = ResizableLabel("G", 0.3)
+        self.b = ResizableLabel("B", 0.3)
 
         self.add_widget(self.button_layout)
         self.add_widget(self.image)
@@ -119,6 +307,11 @@ class CameraPane(BoxLayout):
         self.button_layout.add_widget(self.camera_view)
         self.button_layout.add_widget(self.tracker_view)
         self.button_layout.add_widget(self.reset)
+        self.button_layout.add_widget(self.rgb_layout)
+
+        self.rgb_layout.add_widget(self.r)
+        self.rgb_layout.add_widget(self.g)
+        self.rgb_layout.add_widget(self.b)
 
         self.camera_view.add_callback(self._camera_view_pressed)
         self.tracker_view.add_callback(self._tracker_view_pressed)
@@ -128,6 +321,9 @@ class CameraPane(BoxLayout):
         self.tracker_view_pressed = False
 
         self.camera_view.state = "down"
+
+        self.bind(size=self.update, pos=self.update)
+        Clock.schedule_interval(self._update_rgb, 1/4)
     
     def _camera_view_pressed(self, pressed):
         self.camera_view_pressed = pressed
@@ -145,7 +341,22 @@ class CameraPane(BoxLayout):
         
     def _reset_button_pressed(self, pressed):
         self.image.reset_zoom()
-
+    
+    def update(self, *args):
+        self.r.font_size = self.r.size[1] / 2
+        self.g.font_size = self.g.size[1] / 2
+        self.b.font_size = self.b.size[1] / 2
+    
+    def _update_rgb(self, dt):
+        rgb = self.image.get_rgb()
+        if rgb is not None:
+            self.r.text = str(rgb[0])
+            self.g.text = str(rgb[1])
+            self.b.text = str(rgb[2])
+        else:
+            self.r.text = "R"
+            self.g.text = "G"
+            self.b.text = "B"
 
 class RobotImageBoxButtons(BoxLayout):
     def __init__(self, p_img, p_off_img, dp_img, dp_off_img):
