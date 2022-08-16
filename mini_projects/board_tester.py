c16e82bed2a1c2dc89048ec0c7e6ffff384ada9e
david0x00 <null2@illinois.edu>
2022-08-16 11:55:32 -0500

    test board file ready to roll

--- mini_projects/board_tester.py
+++ mini_projects/board_tester.py
@@ -1,5 +1,6 @@
 import board
 import busio
+import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -17,13 +18,28 @@ sensor_pairs = [(ads_2, ADS.P1), (ads_2, ADS.P0)]
 chan0 = AnalogIn(sensor_pairs[0][0], sensor_pairs[0][1])
 chan1 = AnalogIn(sensor_pairs[1][0], sensor_pairs[1][1])
 
-Va0 = chan0.voltage
-Va1 = chan1.voltage
+for i in range(3):
+    Va0 = chan0.voltage
+    Va1 = chan1.voltage
 
-P0 = ((Va0 / 5.0) + 0.040) / 0.004
-P1 = ((Va1 / 5.0) + 0.040) / 0.004
+    P0 = ((Va0 / 5.0) + 0.040) / 0.004
+    P1 = ((Va1 / 5.0) + 0.040) / 0.004
 
-print("Pressure 0: " + str(P0))
-print("Pressure 1: " + str(P1))
+    print("Pressure 0: " + str(P0))
+    print("Pressure 1: " + str(P1))
+    
+    time.sleep(0.5)
 
-temp_pin
+pin_order = [2, 3, 1, 0]
+pins = []
+for i in pin_order:
+    pin0 = mcp_2.get_pin(i)
+    pin0.switch_to_output(value=False)
+    pins.append(pin0)
+
+for i in range(3):
+    for p in pins:
+        print("Switching Pin")
+        p.value = True
+        time.sleep(2)
+        p.value = False
