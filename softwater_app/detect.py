bd7f316cbaaefe91abb37dade41d066e28acaf3e
peempeem <31141746+peempeem@users.noreply.github.com>
2022-08-17 12:17:16 -0500

    latest

--- softwater_app/detect.py
+++ softwater_app/detect.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy as np
+import time
+import math
+from scipy.spatial import distance_matrix
+from centroid_tracker import CentroidTracker, TrackingPoint
+
 
 class RobotDetector:
     def __init__(self):
-        self.main_color_hue = 168
-        self.main_color_hue_error = 2
+        self.main_color_hue = 175
+        self.main_color_hue_error = 4
         self.main_color_low_sat = 40
         self.main_color_high_sat = 255
         self.main_color_low_val = 100
@@ -14,19 +19,30 @@ class RobotDetector:
         self.params.filterByColor = True
         self.params.blobColor = 255
         self.params.filterByCircularity = True
-        self.params.minCircularity = 0.6
+        self.params.minCircularity = 0.4
         self.params.maxCircularity = float('inf')
         self.params.filterByArea = True
         self.params.minArea = 100
+        self.params.maxArea = float('inf')
         self.params.filterByInertia = True
         self.params.minInertiaRatio = 0.2
         self.params.maxInertiaRatio = float('inf')
+        self.first = True
+        self.timestamp = None
+        self.tracker = CentroidTracker()
         self.detector = cv2.SimpleBlobDetector_create(self.params)
     
     def update_params(self):
         self.detector = cv2.SimpleBlobDetector_create(self.params)
     
-    def detect(self, image):
+    def detect(self, image, prev_pts=[]):
+        if self.first:
+            self.timestamp = time.perf_counter()
+            self.first = False
+        curr = time.perf_counter()
+        dt = curr - self.timestamp
+        self.timestamp = curr
+
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
         low_hue = self.main_color_hue - self.main_color_hue_error
@@ -60,48 +76,70 @@ class RobotDetector:
         mask_blurred = cv2.GaussianBlur(mask, self.gaussian_blur, cv2.BORDER_DEFAULT)
 
         keypoints = self.detector.detect(mask_blurred)
-        detection = cv2.drawKeypoints(mask_blurred, keypoints, np.zeros((1, 1)), (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        return keypoints, detection
+
+        pts = []
+        for pt in keypoints:
+            pts.append(TrackingPoint(pt.pt[0], pt.pt[1], size=pt.size))
+        
+        show_mask = cv2.cvtColor(mask_blurred, cv2.COLOR_GRAY2BGR)
+        
+
+        objects = self.tracker.update(pts, dt)
+
+        detection = self.draw_objects(show_mask, objects)
+        
+
+        return pts, detection
+    
+    def find_indices(self, num, numbers):
+        indices = []
+        for i in range(len(numbers)):
+            if num == numbers[i]:
+                indices.append(i)
+        return indices
+
+    
+    def draw_keypoints(self, image, pts, color=(0, 255, 0)):
+        for pt in pts:
+            cv2.circle(image, pt.int_xy(), pt.size, color, 2)
+        return image
+    
+    def draw_objects(self, image, objects, color=(0, 255, 0)):
+        for obj_id in objects.keys():
+            cv2.circle(image, objects[obj_id].int_xy(), int(objects[obj_id].size), color, 2)
+        return image
 
 
 if __name__ == "__main__":
-    from datalink import DataLink
-    from rate import Rate
-    import time
-
-    link = DataLink("Link1", False, "169.254.11.63")
     detector = RobotDetector()
 
+    cap = cv2.VideoCapture("./data/robot_test.mp4")
+
+    detector.main_color_hue = 175
+    detector.main_color_hue_error = 5
+
+    pts = []
+
     start = time.time()
-
-    main_rate = Rate(200)
-    ask_rate = Rate(30)
-
-    back = True
-
-    link.send({'command': 'get keyframe'})
-
+    count = 0
+    popped = False
     while True:
-        link.update()
+        ret, img = cap.read()
+        if not ret:
+            break
 
-        if link.data_available():
-            msg = link.get()['data']
-            if 'keyframe' in msg.keys():
-                link.send({'command': 'get keyframe'})
-                print((time.time() - start) * 1000)
-                start = time.time()
-                img, values = msg['keyframe']
-                keypoints, _ = detector.detect(img)
-                blank = np.zeros((1, 1))
-                
-                img = cv2.drawKeypoints(img, keypoints, blank, (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                
-                img = cv2.resize(img, (1280, 720))
-                cv2.imshow("img", img)
-                cv2.waitKey(1)
-                
+        count = count + 1
+        if count < 10:
+            continue
+        count = 0
+
+        pts, detection = detector.detect(img, pts)
+
         
-        main_rate.sleep()
+
+        cv2.imshow('detect', detection)
+        cv2.waitKey(1)
+
         
 
         
