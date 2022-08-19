328bed95ffa2f2df422fd9d066af86fcc35c9080
david0x00 <null2@illinois.edu>
2022-08-19 10:58:59 -0500

    autompc controller

---
+++ softwater_app/ampc_controller.py
@@ -0,0 +1,42 @@
+from controller import Controller
+import pickle
+import autompc as ampc
+from autompc.costs import ThresholdCost
+
+
+class AMPCController(Controller):
+    controller_file = "/home/pi/Desktop/acc40/controllers/ampc1_comb.pkl"
+
+    def __init__(self):
+        super().__init__()
+        with open(self.controller_file, "rb") as f:
+            self.mpc = pickle.load(f)
+        self.system = self.mpc.system
+        self.ocp = ampc.OCP(self.system)
+        self.mpc.model._device = "cpu"
+
+    def on_start(self):
+        print("Controller Start")
+
+        targ = [float(self.target[0]), float(self.target[1])]
+        self.ocp.set_cost(
+            ThresholdCost(
+                system=self.system, goal=targ,
+                threshold=2.0, observations=["M10X", "M10Y"]
+            )
+        )
+        for ctrl in self.system.controls:
+            self.ocp.set_ctrl_bound(ctrl, 0, 1)
+        self.mpc.set_ocp(self.ocp)
+        self.mpc.reset()
+
+    def on_end(self):
+        print("Controller End")
+
+    def evaluate(self, x):
+        u = self.mpc.step(x)
+        return u
+
+controller = AMPCController()
+
+
